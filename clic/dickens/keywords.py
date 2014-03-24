9622b8497f579aa66b2218d331e9b10973a2820b
ReinSi <aezros@localhost.localdomain>
2014-03-24 11:06:22 +0000

    cheshire3 typo

--- clic/dickens/keywords.py
+++ clic/dickens/keywords.py
@@ -7,7 +7,7 @@ from math import log1p
 from cheshire3.document import StringDocument
 from cheshire3.internal import cheshire3Root
 from cheshire3.server import SimpleServer
-from cheshire.baseObjects import Session
+from cheshire3.baseObjects import Session
 
 cheshirePath = os.path.join('HOME', '/home/cheshire')
 
