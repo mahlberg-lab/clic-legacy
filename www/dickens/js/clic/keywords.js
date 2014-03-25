b70caae63eef00217c33d267ed3cbecf4c679d10
Shaun Hare <shaun@shaunhare.co.uk>
2014-03-25 14:56:27 +0000

    removed jquery axaj in favour of homespun

--- www/dickens/js/clic/keywords.js
+++ www/dickens/js/clic/keywords.js
@@ -1,3 +1,38 @@
+(function() {
+    var Lib = {
+        ajax: {
+            xhr: function() {
+                var instance = new XMLHttpRequest();
+                return instance;
+            },
+            getJSON: function(options, callback) {
+                var xhttp = this.xhr();
+                options.url = options.url || location.href;
+                options.data = options.data || null;
+                callback = callback ||
+                    function() {};
+                options.type = options.type || 'json';
+                var url = options.url;
+                if (options.type == 'jsonp') {
+                    window.jsonCallback = callback;
+                    var $url = url.replace('callback=?', 'callback=jsonCallback');
+                    var script = document.createElement('script');
+                    script.src = $url;
+                    document.body.appendChild(script);
+                }
+                xhttp.open('GET', options.url, true);
+                xhttp.send(options.data);
+                xhttp.onreadystatechange = function() {
+                    if (xhttp.status == 200 && xhttp.readyState == 4) {
+                        callback(xhttp.responseText);
+                    }
+                };
+            }
+        }
+    };
+
+    window.Lib = Lib;
+})();
 
 
 /**
@@ -10,24 +45,15 @@
 
         keywordHandler.fetch = function(url,data)
         {
-
+            var baseUrl="http://localhost:8080/"
             var query = decodeURIComponent($.param(data));
             var url = baseUrl + "?"+ query;
 
-            $.ajax({
-                  url: url,
-                  data: data,
-                  success: function(data){
+            Lib.getJSON({url:url},function(data){
 
-                console.log(data);
-              },
-                error:function(xhr,error)
-                {
-                  console.debug(xhr); console.debug(error);
-                }
+              console.log(data);
 
-              })
-
+            })
 
         }
 
