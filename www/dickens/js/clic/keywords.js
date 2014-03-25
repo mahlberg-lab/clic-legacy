2476b27f7dff934e911f25090dc4fd9ab9540f63
Shaun Hare <shaun@shaunhare.co.uk>
2014-03-25 17:58:36 +0000

    trying another go at json output

--- www/dickens/js/clic/keywords.js
+++ www/dickens/js/clic/keywords.js
@@ -1,45 +1,10 @@
-(function() {
-    var Lib = {
-        ajax: {
-            xhr: function() {
-                var instance = new XMLHttpRequest();
-                return instance;
-            },
-            getJSON: function(options, callback) {
-                var xhttp = this.xhr();
-                options.url = options.url || location.href;
-                options.data = options.data || null;
-                callback = callback ||
-                    function() {};
-                options.type = options.type || 'json';
-                var url = options.url;
-                if (options.type == 'jsonp') {
-                    window.jsonCallback = callback;
-                    var $url = url.replace('callback=?', 'callback=jsonCallback');
-                    var script = document.createElement('script');
-                    script.src = $url;
-                    document.body.appendChild(script);
-                }
-                xhttp.open('GET', options.url, true);
-                xhttp.send(options.data);
-                xhttp.onreadystatechange = function() {
-                    if (xhttp.status == 200 && xhttp.readyState == 4) {
-                        callback(xhttp.responseText);
-                    }
-                };
-            }
-        }
-    };
-
-    window.Lib = Lib;
-})();
 
 
 /**
  * Keyword handler javascript
  *
  */
-(function (keywordHandler, $, _,Lib,undefined) {
+(function (keywordHandler, $, _,undefined) {
 
 
 
@@ -49,7 +14,7 @@
             var query = decodeURIComponent($.param(data));
             var url = baseUrl + "?"+ query;
 
-            Lib.getJSON({url:url},function(data){
+            $.getJSON(url,function(data){
 
               console.log(data);
 
@@ -59,4 +24,4 @@
 
 
 
-}(window.keywordHandler = window.keywordHandler ||{},jQuery,_,Lib));
+}(window.keywordHandler = window.keywordHandler ||{},jQuery,_));
