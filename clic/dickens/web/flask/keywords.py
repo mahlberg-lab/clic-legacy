3eebee1410766e97a1c94f63970bb6b57171aac7
ReinSi <aezros@localhost.localdomain>
2014-03-26 10:44:21 +0000

    flask

--- clic/dickens/web/flask/keywords.py
+++ clic/dickens/web/flask/keywords.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
@@ -23,45 +24,61 @@ cache_opts = {
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
 
-
-
-
-
 @app.route('/',methods=['GET'])
 def index():
-    args = request.args
-    fetchKeywords(args)
-    return "<span style='color:red'>I am app 1</span>"
+    args = request.args    
+    
+    # put keywords into json
+    keyword_result = fetchKeywords(args)
+    return json.dumps(keyword_result)
 
-
-@cache.cache('keyword', expire=3600) ## expires after 3600 secs
-def fetchKeywords(args):
+#@cache.cache('keyword', expire=3600) ## expires after 3600 secs
+## get keywords from dickens/keywords.py
+def fetchKeywords(args):           
 
     keyworder = Keywords()
     args = processArgs(args)
     keywords = keyworder.list_keywords(args[0], args[1], args[2], args[3])
+    print keywords
 
+    #print {"keywords":keywords}
     return {"keywords":keywords}
+    
+   
 
 def processArgs(args):
+    
+    keywordArgs = []
+    testMod = str(args["testIdxMod"])
+    Group = str(args["testIdxGroup"])
+    refMod = str(args["refIdxMod"])
+    #testIdxName = "{0}-{1}".format(args["testIdxMod"], args["testIdxGroup"])
+    #refIdxName = "{0}-{1}".format(args["refIdxMod"], args["refIdxGroup"])
+    testIdxName = "{0}-{1}".format(testMod, Group)
+    refIdxName = "{0}-{1}".format(refMod, Group)
+    
+    ## if no ngram is specified the index is specific to Mod. If Mod is not specified default to sentence idx
+    print args["testIdxGroup"]
+    
+    if not re.match('\dgram-idx', Group):
+        if not args["testIdxMod"] == '':
+#             testIdxName = args["testIdxMod"] + '-idx'
+#             refIdxName = args["refIdxMod"] + '-idx'
+            testIdxName = testMod + '-idx'
+            refIdxName = refMod + '-idx'
+            
+        else:
+            testIdxName = 'sentence-idx'
+            refIdxName = 'sentence-idx'
+    print testIdxName, refIdxName
+    keywordArgs.insert(0, testIdxName)
+    keywordArgs.insert(2, refIdxName)
 
-  ## if no ngram is specified the index is specific to Mod. If Mod is not specified default to sentence idx
-  if not re.match('\dgram-idx', args["testIdxGroup"]):
-      if not testIdxMod == '':
-          testIdxName = args["testIdxMod"] + '-idx'
-          refIdxName = args["refIdxMod"] + '-idx'
-      else:
-          testIdxName = 'sentence-idx'
-          refIdxName = 'sentence-idx'
-  args.insert(0, testIdxName)
-  args.insert(2, refIdxName)
+    book_collection = [args["testCollection"]]
+    refbook_collection = [args["refCollection"]]
 
 
-  book_collection = [args["testCollection"]]
-  refbook_collection = [args["refCollection"]]
+    keywordArgs.insert(1, book_collection)
+    keywordArgs.insert(3, refbook_collection)
 
-
-  args.insert(1, book_collection)
-  args.insert(3, refbook_collection)
-
-  return args
+    return keywordArgs
