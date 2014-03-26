9d663e0cc81a581cae1c2217206d121a32f631bd
Shaun Hare <shaun@shaunhare.co.uk>
2014-03-26 11:18:54 +0000

    flask keywords template

--- clic/dickens/web/flask/keywords.py
+++ clic/dickens/web/flask/keywords.py
@@ -26,51 +26,52 @@ cache = CacheManager(**parse_cache_config_options(cache_opts))
 
 @app.route('/',methods=['GET'])
 def index():
-    args = request.args    
-    
+    args = request.args
+
     # put keywords into json
     keyword_result = fetchKeywords(args)
-    return json.dumps(keyword_result)
+    keywords = json.dumps(keyword_result)
+
+    return render_template('keywords.html', keywords=keywords)
 
 #@cache.cache('keyword', expire=3600) ## expires after 3600 secs
 ## get keywords from dickens/keywords.py
-def fetchKeywords(args):           
+def fetchKeywords(args):
 
     keyworder = Keywords()
     args = processArgs(args)
     keywords = keyworder.list_keywords(args[0], args[1], args[2], args[3])
     print keywords
 
-    #print {"keywords":keywords}
+
     return {"keywords":keywords}
-    
-   
+
+
 
 def processArgs(args):
-    
+
     keywordArgs = []
     testMod = str(args["testIdxMod"])
     Group = str(args["testIdxGroup"])
     refMod = str(args["refIdxMod"])
-    #testIdxName = "{0}-{1}".format(args["testIdxMod"], args["testIdxGroup"])
-    #refIdxName = "{0}-{1}".format(args["refIdxMod"], args["refIdxGroup"])
+
+
+
     testIdxName = "{0}-{1}".format(testMod, Group)
     refIdxName = "{0}-{1}".format(refMod, Group)
-    
+
     ## if no ngram is specified the index is specific to Mod. If Mod is not specified default to sentence idx
-    print args["testIdxGroup"]
-    
     if not re.match('\dgram-idx', Group):
         if not args["testIdxMod"] == '':
 #             testIdxName = args["testIdxMod"] + '-idx'
 #             refIdxName = args["refIdxMod"] + '-idx'
             testIdxName = testMod + '-idx'
             refIdxName = refMod + '-idx'
-            
+
         else:
             testIdxName = 'sentence-idx'
             refIdxName = 'sentence-idx'
-    print testIdxName, refIdxName
+
     keywordArgs.insert(0, testIdxName)
     keywordArgs.insert(2, refIdxName)
 
