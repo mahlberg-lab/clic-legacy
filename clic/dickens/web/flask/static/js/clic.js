addd92e8b242efc22f87b5c6906479f412466543
Wilson Peter <uizpmw1@exmail.nottingham.ac.uk>
2014-04-10 12:21:46 +0100

    adding bootstrap files

--- clic/dickens/web/flask/static/js/clic.js
+++ clic/dickens/web/flask/static/js/clic.js
@@ -1,56 +1,17 @@
 /**
- *  name: jsonHandler
- *  authors: Rein Shaun
- *  date: 28.03.2014
- *  description: handles json processing for the clic project
+ * Created by uizpmw1 on 07/04/2014.
  */
-(function (jsonHandler, $, _,undefined) {
 
+// 1000ths separators
+function addCommas(nStr) {
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+    }
+    return x1 + x2;
+}
 
-		/**
-		* name:processor
-		* description public method to load the template
-		* params: object data - (json data typically returned from clic.py end points and stored in appropriately named variable in document
-		*		  string dataName name of json variable
-		* 
-		*/
-        jsonHandler.processor = function(data,dataName)
-        {
-
-			
-            getTemplate(data,dataName);
-
-
-        }
-		
-		/**
-		* name:getTemplate
-		* description private method: compiles a template using the data provided.
-		* params: data object - (json data typically returned from clic.py end points and stored in appropriately named variable in document
-		*		  string dataName name of json variable
-		* 
-		*/
-        getTemplate= function(data,dataName){
-
-          var tmplMarkup = $('#tmpl-'+dataName).html();
-
-              if(_.isEmpty(data)){
-
-                      $('#'+dataName).html("<p> no "+dataName+"</p>");
-                  }
-                  else
-                  {
-                   
-                   var templateData={dataItems:data[dataName]};	
-                  
-                   var compiledTmpl = _.template(tmplMarkup, templateData);
-				  
-                  $('#'+dataName).html(compiledTmpl);
-
-                }
-
-        }
-
-
-
-}(window.jsonHandler = window.jsonHandler ||{},jQuery,_));
