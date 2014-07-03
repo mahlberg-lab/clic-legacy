d57b58aba0a893c471d27f74668cda18268f6fba
ReinSi <aezros@localhost.localdomain>
2014-07-03 10:51:23 +0100

    document api file

--- clic/dickens/concordance_new.py
+++ clic/dickens/concordance_new.py
@@ -25,29 +25,34 @@ class Concordancer_New(object):
         self.qf = self.db.get_object(self.session, 'defaultQueryFactory')
         self.resultSetStore = self.db.get_object(self.session, 'resultSetStore')        
         self.idxStore = self.db.get_object(self.session, 'indexStore')
-        ## self.logger = self.db.get_object(self.session, 'concordanceLogger') ## TODO: add to dbs/dickens/config        
+        self.logger = self.db.get_object(self.session, 'concordanceLogger')      
        
+    ## main concordance method
+    ## create a list of lists containing each three contexts left - node -right, 
+    ## and a list within those contexts containing each word. Add two separate lists containing metadata information:
+    ## [ [left context - word 1, word 2, etc.], [node - word 1, word 2, etc], [right context - word 1, etc], 
+    ##   [chapter metadata], [book metadata]
+    ## ], 
+    ## etc.
     def create_concordance(self, terms, idxName, Materials, selectWords): 
-        ## create a list of lists containing each three contexts, and a list within those contexts containing each word
+        ##self.logger.log(10, 'CREATING CONCORDANCE FOR RS: {0} in {1} - {2}'.format(terms, idxName, Materials)) 
         session = self.session
         db = self.db
         qf = self.qf        
         
-        conc_lines = []
-        wordWindow = 10    
+        conc_lines = [] # return concordance lines in list
+        wordWindow = 10 # wordWindow is set to 10 by default - on both sides of node
     
         books = []
         for Material in Materials:
             MatIdx = 'book-idx'
-            #if Material in ['dickens']:
-                #for book in Dickens_vol:
-                    #books.append('c3.{0} = "{1}"'.format(MatIdx, book))  
             if Material in ['dickens', 'ntc']:
                 MatIdx_Vol = 'subCorpus-idx'
                 books.append('c3.{0} = "{1}"'.format(MatIdx_Vol, Material))
             else:
                 books.append('c3.{0} = "{1}"'.format(MatIdx, Material)) 
-                
+            
+        ## search whole phrase or individual words?
         if selectWords == "whole":
             nodeLength = len(terms.split(' '))
             terms = [terms]  
@@ -55,44 +60,55 @@ class Concordancer_New(object):
             nodeLength = 1
             terms = terms.split(' ')
         
+        ## define search term
         term_clauses = []
         for term in terms:
             term_clauses.append('c3.{0} = "{1}"'.format(idxName, term))      
         
-        ## /proxInfo needed to search individual books
+        ## conduct database search
+        ## note: /proxInfo needed to search individual books
         query = qf.get_query(session, ' or '.join(books) + ' and/proxInfo ' + ' or '.join(term_clauses))         
         rs = db.search(session, query) 
     
-        if len(rs) > 0:
-            count = 0
+        ## get total number of hits (not yet used in interface)  
+        total_count = 0        
+        if len(rs) > 0:   
+            for i in rs:                
+                total_count = total_count + len(i.proxInfo)   
+        
+        ## search through each record (chapter) and identify location of search term(s)
+        if len(rs) > 0:            
+            count = 0 ## count hits
             for i in rs:
                  
+                ## get xml record 
                 rec = i.fetch_record(session)
                 tree = rec.get_dom(session).getroottree()        
-                
+                  
                 for m in i.proxInfo: 
                     count += 1
-                    
-                    if count > 1000:
+                      
+                    if count > 1000: ## current search limit: 1000
                         break
                     else:
-                     
+                       
                         if idxName in ['chapter-idx']:    
                             w = m[0][1]                                          
-                     
+                       
                         elif idxName in ['quote-idx', 'non-quote-idx', 'longsus-idx', 'shortsus-idx']:  
-                            #elems = [0] 
                             (e_q, w_q) = (m[0][0], m[0][1])                    
-                              
+                                
                             ## locate search term in xml
-                            search_term = tree.xpath('//*[@eid="%d"]/following::w[%d+1]' % (e_q, w_q))   
-      
+                            search_term = tree.xpath('//*[@eid="%d"]/following::w[%d+1]' % (e_q, w_q)) 
+        
+                            ## get xml of sentence
                             sentence_tree = tree.xpath('//*[@eid="%d"]/following::w[%d+1]/ancestor-or-self::s' % (e_q, w_q))    
                             chapter_tree = tree.xpath('//*[@eid="%d"]/following::w[%d+1]/ancestor-or-self::div' % (e_q, w_q))                         
-                             
+                               
+                            ## counts words preceding sentence   
                             prec_s_tree = chapter_tree[0].xpath('//div//s[@sid="%s"]/preceding::s/descendant::w' % sentence_tree[0].get('sid'))
                             prec_s_wcount = len(prec_s_tree)
-      
+        
                             ## count words within sentence
                             count_s = 0                        
                             for word in chapter_tree[0].xpath('//div//s[@sid="%s"]/descendant::w' % sentence_tree[0].get('sid')):
@@ -100,12 +116,12 @@ class Concordancer_New(object):
                                     count_s += 1
                                 else:
                                     break
-      
-                            wcount = prec_s_wcount + count_s
-                                          
+        
+                            ## word number within chapter is adding word count in preceding sentence and word count in current sentence
+                            wcount = prec_s_wcount + count_s                                            
                             w = wcount                
-      
-                      
+        
+                        
                     ## Define leftOnset as w - 10, then get all w and n between that and node
                     wordWindow = int(wordWindow)
                     leftOnset = max(1, w-wordWindow+1) ## we operate with word position, not list position (word 1 = 0 position in list)
@@ -115,10 +131,10 @@ class Concordancer_New(object):
                         rightOnset = nodeOffset + 1
                     except:
                         rightOnset = None
-                           
+                             
                     ch_words = len(tree.xpath('//div/descendant::w')) ## move to level for each record (chapter) ?                      
                     rightOffset = min(rightOnset + wordWindow, rightOnset + (ch_words - rightOnset) + 1 )
-                        
+                          
                     left_text = []   
                     for l in range(leftOnset, nodeOnset):
                         try:
@@ -131,8 +147,8 @@ class Concordancer_New(object):
                         except:
                             left_n_fo = ''                 
                         left_text.append(''.join(left_n_pr + left_w + left_n_fo))
-                              
-                               
+                                
+                                 
                     node_text = [] 
                     for n in range(nodeOnset, rightOnset):
                         try:
@@ -145,7 +161,7 @@ class Concordancer_New(object):
                         except:
                             node_n_fo
                         node_text.append(''.join(node_n_pr + node_w + node_n_fo))
-                                            
+                                              
                     right_text = [] 
                     for r in range(rightOnset, rightOffset): 
                         try:
@@ -158,14 +174,14 @@ class Concordancer_New(object):
                         except:
                             right_n_fo = ''
                         right_text.append(''.join(right_n_pr + right_w + right_n_fo))
-                      
+                        
                     ### 
                     book = tree.xpath('//div')[0].get('book')
                     chapter = tree.xpath('//div')[0].get('num')
                     para_chap = tree.xpath('//div//descendant::w[%d+1]/ancestor-or-self::p' % w)[0].get('pid')
                     sent_chap = tree.xpath('//div//descendant::w[%d+1]/ancestor-or-self::s' % w)[0].get('sid')
                     word_chap = w                 
-                      
+                        
                     ## count paragraph, sentence and word in whole book
                     count_para = 0
                     count_sent = 0
@@ -174,10 +190,10 @@ class Concordancer_New(object):
                     total_word = []
                     for b in booklist:
                         if b[0][0] == book:
-                              
+                                
                             booktitle.append(b[0][1])
                             total_word.append(b[1][0][2])
-                                        
+                                          
                             for j, c in enumerate(b[2]):
                                 while j+1 < int(chapter):
                                     count_para = count_para + int(c[0])
@@ -185,25 +201,26 @@ class Concordancer_New(object):
                                     count_word = count_word + int(c[2])
                                     j += 1
                                     break
-                                  
+                                    
                                 ## total word in chapter
                                 if j+1 == int(chapter):
                                     chapWordCount = b[2][j][2]
-                      
+                        
                     book_title = booktitle[0]   ## get book title 
                     total_word = total_word[0]     
                     para_book = count_para + int(para_chap)       
                     sent_book = count_sent + int(sent_chap)  
                     word_book = count_word + int(word_chap)                     
- 
+   
                     conc_line = [left_text, node_text, right_text,
                                 [book, book_title, chapter, para_chap, sent_chap, str(word_chap), str(chapWordCount)],
                                 [str(para_book), str(sent_book), str(word_book), str(total_word)]]
-                      
-                      
+                        
+                        
                     conc_lines.append(conc_line)
-  
+    
         conc_lines.insert(0, len(conc_lines))  
+        #conc_lines.insert(0, total_count)  
         return conc_lines
     
                 
--- clic/dickens/web/flask/api.py
+++ clic/dickens/web/flask/api.py
@@ -4,12 +4,9 @@ import json
 
 app = Flask(__name__,static_url_path='')
 
-import re
-
 ## Use beaker to save search (cache). See documentation on http://beaker.readthedocs.org/en/latest/caching.html
 from beaker.cache import CacheManager
 from beaker.util import parse_cache_config_options
-
 
 from clic.dickens.keywords import Keywords
 from clic.dickens.clusters import Clusters
@@ -17,7 +14,6 @@ from clic.dickens.concordance_new import Concordancer_New
 
 from flask import request
 from flask import render_template
-
 
 cache_opts = {
     'cache.type': 'file',
@@ -27,26 +23,24 @@ cache_opts = {
 
 cache = CacheManager(**parse_cache_config_options(cache_opts))
 
-@app.route('/keywords/',methods=['GET'])
+@app.route('/keywords/', methods=['GET'])
 def keywords():
+    ## get search specifications (args):
     args = request.args
-    # put keywords into json
-    keyword_result = fetchKeywords(args)
-    #print keyword_result
-    keywords = json.dumps(keyword_result)
+    ## put keywords into json:
+    keyword_result = fetchKeywords(args) # get list of keywords 
+    keywords = json.dumps(keyword_result) # return keyword list as json
     return keywords
 
-## ajax route: for splitting screen
-@app.route('/ajax-keywords',methods=['GET'])
-def ajax_keyords():
-    args = request.args
-    #return json.dumps(fetchKeywords(args))
-
+## ajax route: not in use here
+# @app.route('/ajax-keywords',methods=['GET'])
+# def ajax_keyords():
+#     args = request.args
+#     #return json.dumps(fetchKeywords(args))
 
 @app.route('/clusters/', methods=['GET'])
 def clusters():
-    args = request.args
-    
+    args = request.args    
     clusters_result = fetchClusters(args)    
     clusters = json.dumps(clusters_result)
     return clusters
@@ -54,30 +48,24 @@ def clusters():
 @app.route('/concordances/',methods=['GET'])
 def concordances():
     args = request.args  
-
     concordances_result = fetchConcordance(args)
     concordances = json.dumps(concordances_result)
     return concordances
 
-
-#@cache.cache('keyword', expire=3600) ## expires after 3600 secs
+#@cache.cache('keywords', expire=3600) ## expires after 3600 secs
 def fetchKeywords(args):
 
     keyworder = Keywords()
     args = processArgs(args, 'keywords')
-    print args
     keywords = keyworder.list_keywords(args[0], args[1], args[2], args[3], args[4])
     return {'keywords':keywords}
 
-#@cache.cache('cluster', expire=3600)
+#@cache.cache('clusters', expire=3600)
 def fetchClusters(args):
 
-    cluster = Clusters()
-    
-    args = processArgs(args, 'clusters')
- 
+    cluster = Clusters()    
+    args = processArgs(args, 'clusters') 
     clusterlist = cluster.list_clusters(args[0], args[1])
-
     return {'clusters' : clusterlist}
 
 #@cache.cache('concordances', expire=3600)
@@ -86,7 +74,6 @@ def fetchConcordance(args):
     concordancer = Concordancer_New()
     args = processArgs(args, 'concordances')
     concordances = concordancer.create_concordance(args[0], args[1], args[2], args[3])
-
     return {'concordances' : concordances}
 
 def processArgs(args, method):
@@ -109,7 +96,6 @@ def processArgs(args, method):
         
 
     if method == 'keywords':
-        #testMod = str(args["testIdxMod"])
         Group = str(args['testIdxGroup']) 
         if not str(args["testIdxMod"]) == 'chapter':
             testMod = str(args["testIdxMod"])            
@@ -117,11 +103,10 @@ def processArgs(args, method):
         else:
             testMod = ''
             testIdxName = "{0}".format(Group)
-#         Group = str(args['testIdxGroup']) ## testGroup not used in concordances
-#         testIdxName = "{0}-{1}".format(testMod, Group)
+
         methodArgs.insert(0, testIdxName)
-        book_collection = args.getlist('testCollection') ## args is a multiDictionary: use .getlist() to access individual books
-        methodArgs.insert(1, book_collection)
+        book_collection = args.getlist('testCollection') 
+        methodArgs.insert(1, book_collection) ## test corpus
 
         refbook_collection = args.getlist('refCollection') 
         if not str(args["refIdxMod"]) == 'chapter':
@@ -131,34 +116,23 @@ def processArgs(args, method):
             refMod = ''
             refIdxName = "{0}".format(Group)
 
-        ## if no ngram is specified the index is specific to Mod. If Mod is not specified default to sentence idx
-        ## THERE WILL BE DEFAULT SELECT IN INTERFACE
-#         if not re.match('\dgram-idx', Group):
-#             if not args['testIdxMod'] == '':
-#                 testIdxName = testMod + '-idx'
-#                 refIdxName = refMod + '-idx'
-# 
-#         else:
-#             testIdxName = 'sentence-idx'
-#             refIdxName = 'sentence-idx'
-
         pValue = str(args['pValue'])
         
         methodArgs.insert(2, refIdxName)
-        methodArgs.insert(3, refbook_collection)
+        methodArgs.insert(3, refbook_collection) ## ref corpus
         methodArgs.insert(4, pValue)
 
     elif method == 'concordances':
 
         testMod = str(args["testIdxMod"])
         testIdxName = testMod + '-idx'
-        #wordWindow = str(args['wordWindow'])
+        #wordWindow = str(args['wordWindow']) ## activate when in future wordWindow is in search options
         book_collection = args.getlist('testCollection')
         select_words = str(args['selectWords'])
 
         methodArgs.insert(0, str(args['terms']))
         methodArgs.insert(1, testIdxName)
-        #methodArgs.insert(2, wordWindow) ## wordwindow set to 10 by default
+        #methodArgs.insert(2, wordWindow) 
         methodArgs.insert(2, book_collection)    
         methodArgs.insert(3, select_words)  
 
