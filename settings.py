14b764b659b24e93719a800880e37b778176e583
jdejoode <jdj@email.com>
2015-01-23 15:18:59 +0100

    Move `data` configuration to a separate settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+# This file contains the project wide settings. It is not
+# part of version control and it should be adapted to
+# suit each deployment. 
+
+
+# Use the absolute path to the directory that stores the data.
+# This can differ per deployment
+DATA_DIRECTORY = "/cheshire3/clic/dbs/dickens/data/"
+
+
