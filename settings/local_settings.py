e4d0fb257c6640cf5b87f8031f7d8920944f3692
jdejoode <jdj@email.com>
2015-02-19 12:41:16 +0000

    Add settings management and document it

---
+++ settings/local_settings.py
@@ -0,0 +1,14 @@
+# This file contains the project wide settings for local dev. 
+# It should be adapted to suit each local setup. 
+
+### For run.py
+
+# Use the absolute path to the directory that stores the data.
+# This can differ per deployment
+DATA_DIRECTORY = ""
+
+
+### For the web app itself
+
+# CACHE_DIR = ""
+# CACHE_LOCK = ""
