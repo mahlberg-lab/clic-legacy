73565958132f011c9d05d516cdeb7e5b8056a57b
Carl Ebrey <carl@ebrey.net>
2015-04-14 14:26:43 +0100

    Refactored chapter repository and added new function which returns the chapter with a given sentence highlighted.

--- clic/chapter_repository.py
+++ clic/chapter_repository.py
@@ -5,6 +5,7 @@ from cheshire3.baseObjects import Session
 from cheshire3.document import StringDocument
 from cheshire3.internal import cheshire3Root
 from cheshire3.server import SimpleServer
+from lxml import etree
 
 BASE_DIR = os.path.dirname(__file__)
 raw_booklist = open(os.path.join(BASE_DIR, 'booklist.json'), 'r')
@@ -21,6 +22,13 @@ class ChapterRepository(object):
         self.db = self.serv.get_object(self.session, self.session.database)
         self.qf = self.db.get_object(self.session, 'defaultQueryFactory')
         
+    def get_book_title(self, book):
+        for b in booklist:
+                if (b[0][0] == book):
+                    book_title = b[0][1]
+
+        return book_title
+
     def get_chapter(self, chapter_number, book):
         """ Returns transformed XML for given chapter & book """
         query = self.qf.get_query(self.session, 'c3.book-idx = "%s"' % book)
@@ -29,11 +37,11 @@ class ChapterRepository(object):
         chapter = chapter_ptr.fetch_record(self.session)
         transformer = self.db.get_object(self.session, 'chapterView-Txr')
         formatted_chapter = transformer.process_record(self.session, chapter).get_raw(self.session)
-        for b in booklist:
-            if (b[0][0] == book):
-                book_title = b[0][1]
-        return formatted_chapter, book_title
+
+        book_title = self.get_book_title(book)
         
+        return formatted_chapter, book_title
+
     def get_raw_chapter(self, chapter_number, book):
         """ Returns raw chapter XML for given chapter & book """
         query = self.qf.get_query(self.session, 'c3.book-idx = "%s"' % book)
@@ -42,4 +50,13 @@ class ChapterRepository(object):
         chapter = chapter_ptr.fetch_record(self.session)
         return chapter.get_dom(self.session)
 
-        
+    def get_chapter_with_highlighted_sentence(self, chapter_number, book, sid):
+        raw_chapter = self.get_raw_chapter(chapter_number, book)
+        xslt_doc = etree.parse('/cheshire3/clic/dbs/dickens/xsl/chapterView.xsl')
+        transformer = etree.XSLT(xslt_doc)
+        transformed_chapter = transformer(raw_chapter, sid="'%s'" % sid)
+        book_title = self.get_book_title(book)
+
+        return etree.tostring(transformed_chapter), book_title
+
+
