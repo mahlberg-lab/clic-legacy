11b884b49e46037ffb44c9e6821b682450c07c39
jdejoode <jdj@email.com>
2015-11-11 16:17:53 +0000

    Update fixtures.py to include full name and a CLI argument

--- clic/web/fixtures.py
+++ clic/web/fixtures.py
@@ -1,36 +1,56 @@
+import json
 import os
-from models import db, Subset
-# importing from admin rather than db, because it needs app context
-from admin import app
+import sys
 
+from clic.web.index import app
+from clic.web.models import db, Subset
+
+input_dir = sys.argv[1]
+
+
+BASE_DIR = os.path.dirname(__file__)
+raw_booklist = open(os.path.join(BASE_DIR, '../booknames.json'), 'r')
+booklist = json.load(raw_booklist)
+
+SUBSETS = [
+# all_suspensions,
+'quotes',
+'short_suspensions',
+'long_suspensions',
+'non_quotes',
+'embedded_quotes',
+'extended_quotes',
+'non_suspended_narration',
+# quotes_without_extended_quotes,
+]
 
 if __name__ == '__main__':
 
      # http://stackoverflow.com/a/19008403/2115409
+
      db.init_app(app)
      with app.app_context():
-        # Extensions like Flask-SQLAlchemy now know what the "current" app
-        # is while within this block. Therefore, you can now run........
         # db.create_all()
-        # change to the right folder
-        os.chdir('/Users/johan/projects/annotation/data/annotation/output/DNov/textract/')
 
+        # change to the right folder
+        os.chdir(input_dir)
         os.listdir(os.curdir)
 
         for folder in os.listdir(os.curdir):
-            for input_file in os.listdir(folder):
-                # parse the file names, fi. BH_non_suspended_quotes.txt
-                name = input_file.split('_')[0]  # BH
-                # BH_non_suspended_quotes.txt -> #  BH_non_suspended_quotes
-                # -> #  non suspended quotes
-                kind = input_file.split('.')[0].split('_')[1:]
-                kind = '-'.join(kind)
+            if folder in SUBSETS:
+                for input_file in os.listdir(folder):
+                    # parse the file names, fi. BH_non_suspended_quotes.txt
+                    name = input_file.split('_')[0]  # BH
+                    full_name = booklist[name]
+                    # BH_non_suspended_quotes.txt -> #  BH_non_suspended_quotes
+                    # -> #  non suspended quotes
+                    kind = input_file.split('.')[0].split('_')[1:]
+                    kind = '-'.join(kind)
 
-                with open(folder + '/' + input_file) as input:
-                    contents = input.readlines()
-                    print name, kind, len(contents)
+                    with open(folder + '/' + input_file) as input:
+                        contents = input.readlines()
+                        print name, full_name, kind, len(contents)
 
-                    for itm in contents:
-                        db.session.add(Subset(book=name, abbr=name, kind=kind, text=itm))
-
-        db.session.commit()
+                        for itm in contents:
+                            db.session.add(Subset(book=full_name, abbr=name, kind=kind, text=itm))
+            db.session.commit()
