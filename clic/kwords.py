289d62a6159acd4ab88779444d8776e42adcbf87
jdejoode <jdj@email.com>
2016-04-19 12:50:21 +0100

    Massive refactor to bring keywords to scientific standard

---
+++ clic/kwords.py
@@ -0,0 +1,235 @@
+# -*- coding: utf-8 -*-
+
+'''
+Module to compute keywords (words that are used significantly more frequently
+in one corpus than they are in a reference corpus). 
+
+The statistical measure used is
+Log Likelihood as explained by Rayson and Garside 
+(http://ucrel.lancs.ac.uk/people/paul/publications/rg_acl2000.pdf)
+
+Usage as follows:
+-----------------
+
+    analysis = pd.DataFrame([('a', 2500), ('the', 25000)], columns=('Type', 'Count'))
+    reference = pd.DataFrame([('a', 10), ('the', 10)], columns=('Type', 'Count'))
+
+    result = extract_keywords(analysis, 
+                     reference, 
+                     tokencount_analysis=20, 
+                     tokencount_reference=100, 
+                     round_values=True, 
+                     limit_rows=3)
+    result
+    
+    keywords_to_json(result)
+
+Or using text files as input:
+-----------------------------
+
+    from collections import Counter
+
+    with open('~/data/input/DNov/BH.txt') as inputfile:
+        bh = inputfile.read().split()
+        bh = Counter(bh)
+
+    with open('~/data/input/DNov/OT.txt') as inputfile:
+        ot = inputfile.read().split()
+        ot = Counter(ot)
+        
+    bh_df  = pd.DataFrame(bh.items(), columns=['Type', 'Count'])
+    ot_df  = pd.DataFrame(ot.items(), columns=['Type', 'Count'])
+
+    extract_keywords(bh_df, 
+                     ot_df, 
+                     tokencount_analysis=bh_df.Count.sum(), 
+                     tokencount_reference=ot_df.Count.sum(), 
+                     round_values=True,)
+
+'''
+
+import os
+import operator
+import pandas as pd
+import numpy as np
+
+from cheshire3.document import StringDocument
+from cheshire3.internal import cheshire3Root
+from cheshire3.server import SimpleServer
+from cheshire3.baseObjects import Session
+
+
+def log_likelihood(counts):
+    '''
+    This function uses vector calculations to compute LL values. 
+    
+    Input: dataframe that is formatted as follows:
+    
+        Type, 
+        Count_analysis, 
+        Total_analysis, 
+        Count_ref, 
+        Total_ref
+    
+    Output: dataframe that is formatted as follows:
+    
+         Type, 
+         Count_analysis, 
+         Total_analysis, 
+         Count_ref, 
+         Total_ref, 
+         Expected_count_analysis,
+         Expected_count_ref
+         LL
+         
+    Hapax legomena in the Count_analysis are not deleted and zero values in the 
+    Count_ref are not treated any differently from other values. 
+    '''
+    
+    # compute expected values
+    counts.loc[:,'Expected_count_analysis'] = counts['Total_analysis'] * (counts['Count_analysis'] + counts['Count_ref']) / (counts['Total_analysis'] + counts['Total_ref'])
+    counts.loc[:,'Expected_count_ref'] = counts['Total_ref'] * (counts['Count_analysis'] + counts['Count_ref']) / (counts['Total_analysis'] + counts['Total_ref'])
+
+    # define variables to avoid cluttering
+    a = counts['Count_analysis']
+    exp_a = counts['Expected_count_analysis']
+    b = counts['Count_ref']
+    exp_b = counts['Expected_count_ref']
+
+    # compute log likelihood
+    counts.loc[:,'LL'] = 2*((a * np.log(a/exp_a)) + (b * np.log(b/exp_b)))
+    
+    # log likelihood if count or expected count in the ref corpus are 0
+    counts.loc[(counts.Count_ref == 0) | (counts.Expected_count_ref == 0), 'LL'] = 2*((a * np.log(a/exp_a)) + (b * 0)) 
+
+    # sort the results
+    keywords = counts.sort_values('LL', ascending=False)
+    
+    return keywords
+    
+def extract_keywords(wordlist_analysis, 
+             wordlist_reference,
+             tokencount_analysis,
+             tokencount_reference,
+             p_value=0.0001,
+             exclude_underused=True,
+             freq_cut_off=5,
+             round_values=True,
+             limit_rows=False
+            ):   
+    '''
+    This is the core method for keyword extraction. It provides a number 
+    of handles to select sections of the data and/or adapt the input for the
+    formula. 
+    
+    Input = Two dataframes with columns 'Type', and 'Count' and 
+            two total tokencounts
+    
+    Output = An aligned dataframe which is sorted on the LL value and maybe filter 
+             using the following handles:
+             
+        - p_value: limits the keywords based on their converted p_value. A p_value of 0.0001
+                   will select keywords that have 0.0001 *or less* as their p_value. It is a 
+                   cut-off. One can choose one out of four values: 0.0001, 0.001, 0.01, or 0.05.
+                   If any other value is chosen, it is ignored and no filtering on p_value is done.
+        
+        - exclude_underused: if True (default) it filters the result by excluding tokens that are 
+                             statistically underused.
+                             
+        - freq_cut_off: limits the wordlist_analysis to words that have the freq_cut_off (inclusive) 
+                        as minimal frequency. 5 is a sane default for Log Likelihood. If one does not 
+                        want frequency-based filtering, set a value of 0.
+                        
+        - round_values: if True (default) it rounds the columns with expected frequencies and LL 
+                        to 2 decimals.
+        
+        - limit_rows: if a number (for instance, 100), it limits the result to the number of rows 
+                      specified. If false, rows are not limited. 
+    
+    The defaults are reasonably sane: 
+     - a token needs to occur at least 5 times in the corpus of analysis
+     - a high p-value is set
+     - no filtering of the rows takes place
+     - the underused tokens are excluded
+     - rounding is active
+
+    For more information on the algorithm, cf. log_likelihood().
+    
+    The first column contains the indeces for the original merged dataframe. It does not display
+    a rank and it should be ignored for keyword analysis (to be more precise: it displays the 
+    frequency rank of the token in the corpus of analysis).
+    '''
+
+    # select only the Type and Count columns
+    wordlist_analysis = wordlist_analysis[['Type', 'Count']]
+    wordlist_reference = wordlist_reference[['Type', 'Count']]
+    
+    # limit with a simple frequency cut-off, does not filter the corpus of reference
+    wordlist_analysis = wordlist_analysis.loc[wordlist_analysis.Count >= freq_cut_off]
+
+    # merge and align the two wordlists
+    merged = wordlist_analysis.merge(wordlist_reference, on='Type', how='left', suffixes=('_analysis', '_ref')).fillna(0)
+    
+    # prepare object for computation
+    if not tokencount_analysis:
+        raise IOError, 'You did not provide a total token count for the corpus of analysis'
+    merged.loc[:,'Total_analysis'] = tokencount_analysis
+    
+    if not tokencount_reference:
+        raise IOError, 'You did not provide a total token count for the corpus of reference'
+    merged.loc[:,'Total_ref'] = tokencount_reference
+    keywords = merged.loc[:,['Type', 'Count_analysis', 'Total_analysis', 'Count_ref', 'Total_ref']]
+
+    # compute keyness
+    keywords = log_likelihood(keywords)
+    
+    # over and underused
+    keywords.loc[:,'Use'] = '0'
+    keywords.loc[keywords.Count_analysis > keywords.Expected_count_analysis, 'Use'] = '+'
+    keywords.loc[keywords.Count_analysis < keywords.Expected_count_analysis, 'Use'] = '-'
+
+    if exclude_underused:
+        keywords = keywords.loc[keywords['Use'] == '+',:]
+    
+    # translate LL value to p-value
+    keywords.loc[:,'p'] = 'p >= 0.05'
+    keywords.loc[keywords['LL'] >= 3.84,'p'] = 'p < 0.05'
+    keywords.loc[keywords['LL'] >= 6.63,'p'] = 'p < 0.01'
+    keywords.loc[keywords['LL'] >= 10.83,'p'] = 'p < 0.001'
+    keywords.loc[keywords['LL'] >= 15.13,'p'] = 'p < 0.0001'        
+    
+    # limit the keywords to the p-value
+    # if the p cut-off does not match either of the conditions, no filtering is done
+    if p_value == 0.0001:
+        keywords = keywords[keywords['LL'] >= 15.13]
+    if p_value == 0.001:
+        keywords = keywords[keywords['LL'] >= 10.83]
+    if p_value == 0.01:
+        keywords = keywords[keywords['LL'] >= 6.63]
+    if p_value == 0.05:
+        keywords = keywords[keywords['LL'] >= 3.84]
+        
+    if round_values:
+        keywords = keywords.round({'LL':2, 'Expected_count_analysis':2, 'Expected_count_ref':2})
+    
+    if limit_rows:
+        return keywords.iloc[:limit_rows]
+    
+    return keywords
+    
+def keywords_to_json(keywords):
+    '''
+    Transforms a keywords table into a json array. 
+    It is specifically tailored at the CLiC Dickens interface.
+    '''
+    keywords.loc[:,'Empty'] = ''
+    keywords = keywords[['Empty',
+                        'Type', 
+                        'Count_analysis', 
+                        'Expected_count_analysis', 
+                        'Count_ref', 
+                        'Expected_count_ref',
+                        'LL',
+                        'p',
+                        'Use']]
+    return keywords.to_json(orient='values') 
---
+++ clic/wordlists.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+'''
+Build a wordlist of tokens or phrases using the cheshire3
+database. 
+
+For instance:
+
+    clusters = Cheshire3WordList()
+    clusters.build_wordlist('non-quote-idx', ['ntc'])
+    clusters.total
+    clusters.wordlist
+'''
+
+import copy
+import os
+import pandas as pd
+
+from cheshire3.document import StringDocument
+from cheshire3.internal import cheshire3Root
+from cheshire3.server import SimpleServer
+from cheshire3.baseObjects import Session
+
+
+class Cheshire3WordList(object):
+    '''
+    Main class used to build Cheshire3 word lists. These
+    can be of individual tokens or of clusters (also
+    called n-grams or phrases).
+    '''
+
+    def __init__(self):
+        '''
+        Sets up the connection with Cheshire3. 
+        '''
+        self.session = Session()
+        self.session.database = 'db_dickens'
+        self.serv = SimpleServer(self.session,
+                            os.path.join(cheshire3Root, 'configs', 'serverConfig.xml')
+                            )
+        self.db = self.serv.get_object(self.session, self.session.database)
+        self.qf = self.db.get_object(self.session, 'defaultQueryFactory')
+        self.resultSetStore = self.db.get_object(self.session, 'resultSetStore')
+        self.idxStore = self.db.get_object(self.session, 'indexStore')
+    
+    def build_subcorpus_clauses(self, subcorpora):
+        '''
+        Takes a list of subcorpora and turns it into a 
+        CQL query that Cheshire3 can process.
+        '''
+        
+        if not isinstance(subcorpora, list):
+            raise IOError, 'subcorpora should be a list'
+        
+        clauses = []
+        for subcorpus in subcorpora:
+            if subcorpus in ['dickens', 'ntc']:
+                idx = 'subCorpus-idx'
+            else:
+                idx = 'book-idx'
+            clauses.append('c3.{0} = "{1}"'.format(idx, subcorpus))
+        return clauses
+    
+    def get_facets(self, index_name, subcorpora):
+        '''
+        Get the actual word counts ('facets') using the 
+        index and the list of subcorpora. 
+        '''
+        clauses = self.build_subcorpus_clauses(subcorpora)
+        query = self.qf.get_query(self.session,
+                                       ' or '.join(clauses)
+                                       )
+        results = self.db.search(self.session, query)
+        idx = self.db.get_object(self.session, index_name)
+        facets = idx.facets(self.session, results)
+        return facets
+        
+    def facets_to_df(self, facets):
+        '''
+        Converts the facets into a dataframe that can be manipulated
+        more easily.
+        '''
+        
+        def select_third_value(value):
+            '''
+            Facets come in the following format:
+            [(u'a', (38, 879, 84372)),
+             (u'all', (1067, 879, 15104)),
+             
+            This function returns the third values, respectively 84372 and 15104
+            in the example above.
+            '''
+            return value[2]
+
+        dataframe = pd.DataFrame(facets, columns =['Type', 'Raw facet'])
+        dataframe.index += 1
+        
+        dataframe['Count'] = dataframe['Raw facet'].apply(select_third_value)
+        self.total = dataframe.Count.sum() 
+        dataframe['Percentage'] = dataframe.Count / self.total * 100
+        dataframe['Percentage'] = dataframe['Percentage'].round(decimals=2)
+        dataframe.sort('Count', ascending=False, inplace=True)
+        dataframe['Empty'] = ''
+        return dataframe
+    
+    def wordlist_to_json(self):
+        '''
+        Returns a json string that is 
+        adapted to the CLiC API.
+        '''
+        
+        # do not work on the original
+        wordlist = copy.deepcopy(self.wordlist)
+        del wordlist['Raw facet']
+        wordlist = wordlist[['Empty', 'Type', 'Count', 'Percentage']]
+        return wordlist.to_json(orient='values')
+    
+    def build_wordlist(self, index_name, subcorpora):
+        '''
+        The core method that needs to be called in order to 
+        actually generate the keyword list. Once this method is called
+        the .wordlist attribute will return the wordlist. 
+        '''        
+        facets = self.get_facets(index_name, subcorpora)
+        self.wordlist = self.facets_to_df(facets)
