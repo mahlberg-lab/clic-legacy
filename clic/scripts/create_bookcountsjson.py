b6670f81740782128b1aff3a1d397433901b85d7
jdejoode <jdj@email.com>
2016-05-04 13:50:46 +0100

    Update book counts now including total counts/corpus

--- clic/scripts/create_bookcountsjson.py
+++ clic/scripts/create_bookcountsjson.py
@@ -1,4 +1,95 @@
-## TO CREATE bookcounts.json
+'''
+TO CREATE bookcounts.json
+ 
+Output format:
+ 
+  # [
+  #     ["dickens",
+  #         "All Dickens (15 novels)", [3835807, 1348611, 2487196]],
+  #     [
+  #         [
+  #             "BH",
+  #             "Bleak House",
+  #             [
+  #                 354362,
+  #                 138453,
+  #                 215909
+  #             ],
+  #             [
+  #                 0,
+  #                 2615,
+  #                 5516,
+  #                 13412,
+  #                 18306,
+  #                 24006,
+  #                 32710,
+  #                 36945,
+  #                 45254,
+  #                 51516,
+  #                 55738,
+  #                 61593,
+  #                 67150,
+  #                 73195,
+  #                 81758,
+  #                 88075,
+  #                 91585,
+  #                 97267,
+  #                 104365,
+  #                 109781,
+  #                 115431,
+  #                 122324,
+  #                 127515,
+  #                 134900,
+  #                 142311,
+  #                 145826,
+  #                 150990,
+  #                 155914,
+  #                 160518,
+  #                 164337,
+  #                 170880,
+  #                 177434,
+  #                 182839,
+  #                 188727,
+  #                 194973,
+  #                 201171,
+  #                 207936,
+  #                 215669,
+  #                 219939,
+  #                 226230,
+  #                 230804,
+  #                 234250,
+  #                 237045,
+  #                 243367,
+  #                 246462,
+  #                 251840,
+  #                 255335,
+  #                 261121,
+  #                 267438,
+  #                 272923,
+  #                 277024,
+  #                 281664,
+  #                 286148,
+  #                 290585,
+  #                 299106,
+  #                 305563,
+  #                 308796,
+  #                 316277,
+  #                 321946,
+  #                 327295,
+  #                 332508,
+  #                 336768,
+  #                 340374,
+  #                 343552,
+  #                 348007,
+  #                 350965,
+  #                 352686
+  #             ]
+  #         ],
+  #  ...
+  # 
+  # ["ntc",
+  #     "All 19thC (29 Non-Dickens novels)", [4513076, 1606974, 2906102]],
+'''
 
 import os
 import re
@@ -10,15 +101,11 @@ from cheshire3.internal import cheshire3Root
 from cheshire3.server import SimpleServer
 from cheshire3.baseObjects import Session
 
-
 ### read info from booklist: cumulative word count in chapters
 # booklist = open('/home/aezros/workspace/testClic/staticFiles_test/booklist2')
 # booklist = json.load(booklist)
 # for b1 in booklist:
 #     
-    
-
-###
 
 session = Session()
 session.database = 'db_dickens'
@@ -29,6 +116,9 @@ db = serv.get_object(session, session.database)
 qf = db.get_object(session, 'defaultQueryFactory')
 resultSetStore = db.get_object(session, 'resultSetStore')       
 idxStore = db.get_object(session, 'indexStore')
+ 
+dnov_books = ['BH', 'BR', 'DC', 'DS', 'ED', 'GE', 'HT', 'LD', 'MC', 'NN',
+              'OCS', 'OMF', 'OT', 'PP', 'TTC'] 
  
 list_books = ['BH', 'BR', 'DC', 'DS', 'ED', 'GE', 'HT', 'LD', 'MC', 'NN',
               'OCS', 'OMF', 'OT', 'PP', 'TTC',
@@ -57,7 +147,16 @@ titles = {'BH': 'Bleak House', 'BR': 'Barnaby Rudge', 'DC': 'David Copperfield',
 list_all_books = []
 #list_all_books.insert(0, 'dickens')
  
-within_book = []
+all_dickens = 0
+quotes_dickens = 0
+nonquotes_dickens = 0
+
+all_ntc = 0
+quotes_ntc = 0
+nonquotes_ntc = 0
+
+within_dickens = []
+within_ntc = []
 for b in list_books:    
              
     query = qf.get_query(session, 'c3.book-idx = "%s"' % b)
@@ -104,10 +203,23 @@ for b in list_books:
             book_title = t[1]
             break             
        
-    within_book.append([b, book_title, [all_words, quote_words, nonquote_words], wordCumulative])
-        
+    if b in dnov_books:
+        # update the total counts per corpus
+        all_dickens += all_words
+        quotes_dickens += quote_words
+        nonquotes_dickens += nonquote_words
+        # add the count per book
+        within_dickens.append([b, book_title, [all_words, quote_words, nonquote_words], wordCumulative])
+
+    else: 
+        all_ntc += all_words
+        quotes_ntc += quote_words
+        nonquotes_ntc += nonquote_words
+        within_ntc.append([b, book_title, [all_words, quote_words, nonquote_words], wordCumulative])        
     #break
       
-list_all_books.append(within_book)
+list_all_books.append(["dickens", "All Dickens (15 novels)", [all_dickens, quotes_dickens, nonquotes_dickens]])
+list_all_books.append(within_dickens)
+list_all_books.append(["ntc", "All 19thC (29 Non-Dickens novels)", [all_ntc, quotes_ntc, nonquotes_ntc]],)
+list_all_books.append(within_ntc)
 print json.dumps(list_all_books)
- 
