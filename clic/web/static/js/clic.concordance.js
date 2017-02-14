570aa75e4522783e4ef2c9676d5270e5c83aec1f
Jamie Lentin <jm@lentin.co.uk>
2017-02-14 18:36:42 +0000

    clic.concordance.js: Rework to use dataTables more efficiently

--- clic/web/static/js/clic.concordance.js
+++ clic/web/static/js/clic.concordance.js
@@ -33,9 +33,58 @@
         init: function() {
             var that = this;
 
-            var params = location.search;
-            this.processParameters(params);
-            var concordanceUrl = this.options.concordanceEndpointUrl + params;
+            function renderTokenArray( data, type, full, meta ) {
+                //TODO: Generate HTML for nodes
+                return data.join("");
+            }
+
+            function renderPosition( data, type, full, meta ) {
+                var xVal;
+
+                if (type === 'sort') {
+                    return data[2];
+                }
+
+                xVal = (data[2] / data[3]) * 50; // word in book / total word count
+                return '<svg width="50px" height="15px" xmlns="http://www.w3.org/2000/svg">' +
+                       '<rect x="0" y="4" width="50" height="7" fill="#ccc"/>' +
+                       '<line x1="' + xVal + '" x2="' + xVal + '" y1="0" y2="15" stroke="black" stroke-width="2px"/>' +
+                       '</svg>';
+            }
+
+            that.processParameters(document.location.search);
+
+            $('#dataTableConcordance').dataTable({
+                /*TODO:
+                var chapterViewUrl = '/chapter/' + data.concordances[x][3][0] + '/' + data.concordances[x][3][2] + '/' + data.concordances[x][3][5] + '/' + this.searchTerms + '/#concordance';
+                content += '<tr class="clickable_row" data-url="' + chapterViewUrl + '">';
+                */
+
+                ajax: that.fetchData.bind(that),
+                columns: [
+                    //TODO: Counter column?
+                    { title: "Left", data: "0", render: renderTokenArray, class: "text-right" }, // Left //TODO: Custom sort
+                    { title: "Node", data: "1", render: renderTokenArray, class: "hilight" }, // Node //TODO: Custom sort
+                    { title: "Right", data: "2", render: renderTokenArray }, // Right //TODO: Custom sort
+                    { title: "Book", data: "3.1" }, // Book
+                    { title: "Ch.", data: "3.2" }, // Chapter
+                    { title: "Par.", data: "3.3" }, // Paragraph
+                    { title: "Sent.", data: "3.4" }, // Sentence
+                    { title: "In&nbsp;bk.", data: "4", render: renderPosition }, // Book graph TODO: Custom sort
+                ],
+                filter: true,
+                sort: true, //TODO: What should the default sorting be?
+                paginate: true,
+                language: {
+                    search: "Filter concordance:" //TODO: Actually limit it to concordance?
+                },
+                //TODO: TableTools Copy CSV / Print / Toggle metadata?
+                //TODO: Toggle metadata option?
+            });
+        },
+
+        fetchData: function ( data, callback, settings ) {
+            var that = this;
 
             $.when(
                 $.ajax({
@@ -43,21 +92,20 @@
                     type: 'GET',
                     dataType: 'json'}),
                 $.ajax({
-                    url: concordanceUrl,
+                    url: that.options.concordanceEndpointUrl + document.location.search,
                     type: 'GET',
                     dataType: 'json'})
             ).then(
-                function(chaptersData, concordancesData) { // success
-                    var chData = chaptersData[0];
-                    var coData = concordancesData[0];
+                function(chaptersResponse, concordancesResponse) { // success
+                    var chData = chaptersResponse[0];
+                    var coData = concordancesResponse[0].concordances.slice(1);
 
-                    var chapterData = that.processChapterMarkers(chData);
-                    var concordanceContent = that.processConcordanceData(coData);
-                    var concordancePlot = that.processConcordancePlot(coData, chapterData);
+                    $("#searchedFor").html("Searched for <b>" + that.searchTerms + "</b> within <b>" + that.searchSpace + "</b>.");
 
-                    that.renderConcordance(concordanceContent, concordancePlot);
-                    that.setupDataTables();
                     Pace.stop();
+                    callback({ data: coData });
+
+                    $('#plotTbody').html(that.processConcordancePlot(coData, that.processChapterMarkers(chData)));
                 },
                 function(e) { // failure
                     console.log(e);
@@ -124,65 +172,11 @@
             return chapterDataArray;
         },
 
-        processConcordanceData: function( data ) {
-            this.totalNumberOfHits = data.concordances[0];
-
-            var content = '';
-            // loop out values
-            for (var x = 1; x < data.concordances.length; x++) {
-                var wordsLhs = '';
-                var wordsRhs = '';
-                var node = '';
-
-                var chapterViewUrl = '/chapter/' + data.concordances[x][3][0] + '/' + data.concordances[x][3][2] + '/' + data.concordances[x][3][5] + '/' + this.searchTerms + '/#concordance';
-
-                content += '<tr class="clickable_row" data-url="' + chapterViewUrl + '">';
-                // counter
-                content += '<td>' + x + '</td>';
-                // words left
-                for (var s = 0; s < data.concordances[x][0].length; s++) {
-                    wordsLhs += data.concordances[x][0][s] + ' ';
-                }
-
-                content += '<td class="left nowrap" align="right"><span>' + wordsLhs + '</span></td>';
-
-                // node
-                for (var s = 0; s < data.concordances[x][1].length; s++) {
-                    node += data.concordances[x][1][s] + ' ';
-                }
-                content += '<td class="hilight">' + node + '</td>';
-
-                // words right
-                for (var s = 0; s < data.concordances[x][2].length; s++) {
-                    wordsRhs += data.concordances[x][2][s] + ' ';
-                }
-                content += '<td class="right nowrap">' + wordsRhs + '</td>';
-                //Book
-                content += '<td class="book">' + data.concordances[x][3][1] + '</td>';
-                // Chapter
-                content += '<td>' + data.concordances[x][3][2] + '</td>';
-                // Paragraph
-                content += '<td>' + data.concordances[x][3][3] + '</td>';
-                // Sentence
-                content += '<td>' + data.concordances[x][3][4] + '</td>';
-                // % In book
-                var wordInBook = (data.concordances[x][4][2] / data.concordances[x][4][3]) * 100; // word in book / total word count
-                var xVal = Math.round(wordInBook) / 2;
-                content += '<td><svg width="50px" height="15px" xmlns="http://www.w3.org/2000/svg">' +
-                        '<rect x="0" y="4" width="50" height="7" fill="#ccc"/>' +
-                        '<line x1="' + xVal + '" x2="' + xVal + '" y1="0" y2="15" stroke="black" stroke-width="2px"/>' +
-                        '</svg>';
-                content += '</tr></td>';
-            }
-
-            return content;
-        },
-
         processConcordancePlot: function( data, chapterDataArray ) {
             // Add unique book titles to array
             var uniqueBookTitles = [];
-            for (var x = 1; x < data.concordances.length; x++) {
-                var bookTitle = data.concordances[x][3][1];
+            for (var x = 0; x < data.length; x++) {
+                var bookTitle = data[x][3][1];
                 if (uniqueBookTitles.indexOf(bookTitle) < 0) {
                     uniqueBookTitles.push(bookTitle);
                 }
@@ -195,13 +189,13 @@
                 // loop out line values
                 var lines = '';
                 var lineCount = 0; // no of occurrences of word
-                for (var x = 1; x < data.concordances.length; x++) {
+                for (var x = 0; x < data.length; x++) {
                     // if this is the book title we want
-                    if (data.concordances[x][3][1] == uniqueBookTitles[i]) {
+                    if (data[x][3][1] == uniqueBookTitles[i]) {
                         // calculate line values
-                        var totalWordCountInBook = data.concordances[x][4][3];
+                        var totalWordCountInBook = data[x][4][3];
                         var adjustedTotalWordCountInBook = totalWordCountInBook / 1000;
-                        var wordInBook = data.concordances[x][4][2];
+                        var wordInBook = data[x][4][2];
                         var xVal = wordInBook / adjustedTotalWordCountInBook;
 
                         // line here
@@ -227,86 +221,6 @@
 
             return plotContent;
         },
-
-        renderConcordance: function( content, plotContent ) {
-            $('#resultsTbody').html(content);
-            $('#plotTbody').html(plotContent);
-
-            $('#dataTableConcordance tbody').on( 'click', 'tr', function () {
-                window.location = $(this).data('url');
-            });
-
-            $("#searchedFor").html("Searched for <b>" + this.searchTerms + "</b> within <b>" + this.searchSpace + "</b>.");
-
-            $('#concordanceWrap').show();
-        },
-
-        setupDataTables: function() {
-            var that = this;
-
-            $('#dataTableConcordance').dataTable({
-
-                // counter on column0, recounts when filter see http://www.datatables.net/release-datatables/examples/api/counter_column.html
-                fnDrawCallback: function (oSettings) {
-                    var that = this;
-
-                    /* Need to redo the counters if filtered or sorted */
-                    if (oSettings.bSorted || oSettings.bFiltered) {
-                        this.$('td:first-child', {"filter": "applied"}).each(function (i) {
-                            that.fnUpdate(i + 1, this.parentNode, 0, false, false);
-                        });
-                    }
-                },
-                fnInfoCallback: function (oSettings, iStart, iEnd, iMax, iTotal, sPre) {
-                  if (iTotal > 0) {
-                      return iStart + " to " + iEnd + " of " + that.totalNumberOfHits + " entries";
-                  }
-
-                  return "0 entries";
-                },
-                bFilter: true,
-                bSort: true,
-                bPaginate: false,
-                aoColumnDefs: [
-                    { bSortable: false, aTargets: [ 0, 8 ] },
-                    { bSearchable: false, aTargets: [0, 4, 5, 6, 7, 8 ] },
-                    // custom sort Left using L1; right using R1
-                    { sType: "string-L1", aTargets: [ 1 ] },
-                    { sType: "string-R1", aTargets: [ 3 ] }
-                ],
-                // change phrase on search box (as now limited to just concordance)
-                oLanguage: {
-                    sSearch: "Filter concordance:"
-                },
-                // duplicate controls iflp
-                // plus add dataTools eg save
-                sDom: '<iTf>rt<if>',
-                // retrieve: true,
-                oTableTools: {
-                    aButtons: [
-                        "csv",
-                        "print",
-                        {
-                            sExtends: "text",
-                            sButtonText: "Toggle metadata",
-                            fnClick: function (nButton, oConfig, oFlash) {
-                                // show hide columns
-                                /* Get the DataTables object again - this is not a recreation, just a get of the object */
-                                var oTable = $('#dataTableConcordance').dataTable();
-                                // until have time to find a more elegant solution
-                                var index;
-                                var colsList = [5, 6, 7, 8];
-                                for (index = 0; index < colsList.length; ++index) {
-                                    var bVis = oTable.fnSettings().aoColumns[colsList[index]].bVisible;
-                                    oTable.fnSetColumnVis(colsList[index], bVis ? false : true);
-                                }
-                            }
-                        }
-                    ],
-                    sSwfPath: "/js/thirdparty/DataTables-1.10.13/extensions/TableTools/swf/copy_csv_xls.swf"
-                }
-            });
-        }
     };
 
     // A really lightweight plugin wrapper around the constructor,
