cd2dad979ceab9401bc8031f66e8ce359b0e0169
Jamie Lentin <jm@lentin.co.uk>
2017-04-12 14:24:02 +0000

    clic/index.py: Debug options cause large responses to be dropped, remove them by default

--- clic/web/index.py
+++ clic/web/index.py
@@ -684,5 +684,4 @@ if __name__ == '__main__':
     def initialize_database():
         db.create_all()
 
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
